8a5921a01221dcd094f6b237f5716abe6b832c65
VBSz <szabovili41@gmail.com>
2019-06-29 18:19:44 +0200

    3 files modified

--- src/app/sabw-caesar/sabw-caesar.component.ts
+++ src/app/sabw-caesar/sabw-caesar.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sabw-caesar.component.css']
 })
 export class SabwCaesarComponent implements OnInit {
-
+zahl = 12;
+rotation = 'rotate(318.5deg)';
+ dreheScheibe(){
+  let deg = 360-360/26*this.zahl;
+  this.rotation='rotate(180deg)';
+ }
   constructor() { }
 
   ngOnInit() {
